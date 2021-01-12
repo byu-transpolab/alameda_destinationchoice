bc86e2397818c286effcbd6cae75a551c0c00e3e
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-01-12 15:41:24 -0700

    Add ggmap bbox function for using ggmap on geom_sf

--- R/methods.R
+++ R/methods.R
@@ -1,3 +1,23 @@
+ggmap_bbox <- function(map) {
+    if (!inherits(map, "ggmap")) stop("map must be a ggmap object")
+    # Extract the bounding box (in lat/lon) from the ggmap to a numeric vector,
+    # and set the names to what sf::st_bbox expects:
+    map_bbox <- setNames(unlist(attr(map, "bb")),
+                         c("ymin", "xmin", "ymax", "xmax"))
+    # Coonvert the bbox to an sf polygon, transform it to 3857,
+    # and convert back to a bbox (convoluted, but it works)
+    bbox_3857 <- st_bbox(st_transform(st_as_sfc(st_bbox(map_bbox, crs = 4326)), 3857))
+    # Overwrite the bbox of the ggmap object with the transformed coordinates
+    attr(map, "bb")$ll.lat <- bbox_3857["ymin"]
+    attr(map, "bb")$ll.lon <- bbox_3857["xmin"]
+    attr(map, "bb")$ur.lat <- bbox_3857["ymax"]
+    attr(map, "bb")$ur.lon <- bbox_3857["xmax"]
+    map
+}
+
+
+
+
 #' Methods for mlogit objects
 #'
 #' Miscellaneous methods for `mlogit` objects.
